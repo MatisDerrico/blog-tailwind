2c7291a808b608974cf7462ecbc6622a39b69304
MatisDerrico <matisderrico@hotmail.com>
2023-11-15 16:27:05 +0100

    blog-tailwind-v4

--- main.js
+++ main.js
@@ -1,2 +1,42 @@
-import './style.css'
+import './style.css';
+import data from './public/data.json';
+import { nav } from './nav';
+
+
+data.sort((a,b) => `${a.prenom} ${a.nom}`.localeCompare(`${b.prenom} ${b.nom}`));
+
+const listeArticles = () => {
+  let html = '';
+  for (let i = 0; i < data.length; i++) {
+    const article = data[i];
+    // <img src="${article.avatar}" class="card-img-top" alt="avatar de ${article.prenom} ${article.nom}"> //
+    let articleCard = `
+      <a href="/article/?id=${article.id}">
+       
+        <div>
+          <h5>${article.prenom} ${article.nom}</h5>
+          <p>${article.author}</p>
+          <p>${article.content}</p>
+        </div>
+      </a>
+    `;
+    html += articleCard;
+  }
+  return html;
+};
+
+document.querySelector('#app').innerHTML = `
+  <main>
+    ${nav}
+
+    <div class="container-fluid my-4">
+      <div class="d-flex gap-3 flex-wrap justify-content-center">
+        ${listeArticles()}
+      </div>
+    </div>
+  </main>
+`;
+
+const searchInput = document.querySelector("search");
+searchInput.addEventListener
 
--- nav.js
+++ nav.js
@@ -1,6 +1,7 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
+export const nav = `
+
 <nav class="bg-gray-800">
 <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
   <div class="relative flex h-16 items-center justify-between">
@@ -76,6 +77,4 @@ document.querySelector('#app').innerHTML = `
 </div>
 </nav>
 
-`
-
-setupCounter(document.querySelector('#counter'))
+`;
